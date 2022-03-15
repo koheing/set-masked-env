3d2336c71e98aedd031bd3f76fadbb40dcc6da4e
koheing <koheing.0111@gmail.com>
2022-03-15 22:32:43 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { exportVariable, getInput, setFailed, setSecret } from '@actions/core'
+import { readFile } from 'fs'
+import { promisify } from 'util'
+
+const readFileAsync = promisify(readFile)
+
+async function main() {
+  try {
+    const path = getInput('path')
+    const source = await readFileAsync(path, { encoding: 'utf8' })
+    const lines = source.replaceAll('\r\n', '\n').split('\n')
+    
+    lines.forEach((line) => {
+      const [variable, value] = line.split('=')
+      setSecret(value)
+      exportVariable(variable, value)
+
+      console.log(`${variable}: ${value}`)
+    })
+  } catch (e) {
+    setFailed(e)
+  }
+}
+
+main()
